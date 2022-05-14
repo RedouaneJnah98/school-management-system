c844738359286c6f30c2b05758f32c2610f2c23c
RedouaneJnah98 <redouanejnah2000@gmail.com>
2022-05-14 13:28:04 +0100

    Refactor Database Factory

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -27,7 +27,14 @@ class TeacherFactory extends Factory
             'phone' => $this->faker->phoneNumber(),
             'status' => $this->faker->randomElement(['admin', 'teacher']),
             'profile_bio' => $this->faker->sentence(),
-            'password_confirmation' => Hash::make('12345678')
+            'password_confirmation' => Hash::make('12345678'),
+            'gender' => $this->faker->randomElement(['male', 'female']),
+            'address' => $this->faker->address(),
+            'number' => $this->faker->randomDigit(),
+            'city' => $this->faker->city(),
+            'zip' => $this->faker->randomNumber(5, true),
+            //image profile
+            'profile_image' => $this->faker->imageUrl(50, 50, 'avatar'),
         ];
     }
 }
