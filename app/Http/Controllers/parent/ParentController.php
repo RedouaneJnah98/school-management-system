c3b6171b7060c6e9d2c71aff5260bc6830ad3132
RedouaneJnah98 <redouanejnah2000@gmail.com>
2022-05-20 12:10:13 +0100

    Finish Parent CRUD

--- app/Http/Controllers/parent/ParentController.php
+++ app/Http/Controllers/parent/ParentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\parent;
 use App\Http\Controllers\Controller;
 use App\Models\Parents;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ParentController extends Controller
 {
@@ -37,11 +38,28 @@ class ParentController extends Controller
 
     public function update(Request $request, Parents $parent)
     {
-        //
+        $attributes = $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => ['required', 'email', Rule::unique('parents', 'email')->ignore($parent->id)],
+            'phone_number' => 'required|numeric',
+            'gender' => 'required'
+        ]);
+
+        // update information
+        $update_info = $parent->update($attributes);
+
+        if ($update_info) {
+            return redirect()->route('admin.parents.index')->with('success', 'Parent updated successfully');
+        }
+
+        return redirect()->back()->with('failed', 'Something went wrong, try again.');
     }
 
     public function destroy(Parents $parent)
     {
-        //
+        $parent->delete();
+
+        return redirect()->back()->with('delete', 'Parent and their student accounts deleted.');
     }
 }
