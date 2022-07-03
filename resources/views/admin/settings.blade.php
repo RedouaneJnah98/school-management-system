1c8cb6ad7feb21e88fb6ca639dbf58aef2f845d4
RedouaneJnah98 <redouanejnah2000@gmail.com>
2022-07-03 11:15:43 +0100

    Finish settings page

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -7,16 +7,42 @@
     <main class="content pb-5">
         {{-- Navbar --}}
         @include('components.settings.admin.navbar')
+        {{-- Modal --}}
+        <x-modals.settings_modal/>
 
         <div class="alert alert-success mt-5">
             Edit your account settings and change your password here.
         </div>
+
         <div class="card border-0 shadow mt-2">
             <div class="d-flex justify-content-center align-items-center">
                 <div class="card-body">
-                    <div class="border border-success p-2 mb-2">
-                        Your email address is: <strong>{{ auth()->user()->email }}</strong>
+                    <h6>Email:</h6>
+                    <div class="border border-primary p-2 mb-2 d-flex justify-content-between align-items-center">
+                        <div>
+                            Your email address is: <strong>{{ auth()->user()->email }}</strong>
+                        </div>
+                        <button class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#modal-form">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd"
+                                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                            </svg>
+                            Edit
+                        </button>
                     </div>
+                    <hr>
+                    <h6>Password:</h6>
+                    <form action="">
+                        @csrf
+
+                        <input type="password" class="form-control" placeholder="Enter Current Password">
+                        <input type="password" class="form-control my-3" placeholder="Enter New Password">
+                        <input type="password" class="form-control" placeholder="Re-type New Password">
+
+                        <button type="submit" class="btn btn-primary text-white mt-5">Change Password</button>
+                    </form>
                 </div>
             </div>
         </div>
