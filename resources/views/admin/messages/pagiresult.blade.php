4e1b6785118670299e2c70dfc75365085b8a981f
RedouaneJnah98 <redouanejnah2000@gmail.com>
2022-08-01 13:07:39 +0100

    fix styles pagination.

--- resources/views/admin/messages/pagiresult.blade.php
+++ resources/views/admin/messages/pagiresult.blade.php
@@ -147,13 +147,13 @@
             <div class="col-7 mt-1">Showing {{ $messages->firstItem() }} - {{ $messages->lastItem() }} of {{ $messages->total() }}</div>
             <div class="col-5">
                 <div class="btn-group float-end test">
-                    <a href="{{ $messages->previousPageUrl() }}" class="btn btn-gray-100">
+                    <a href="{{ $messages->previousPageUrl() }}" class="btn btn-gray-100 {{ $messages->onFirstPage() ? 'disabled' : '' }}">
                         <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
                                   clip-rule="evenodd"/>
                         </svg>
                     </a>
-                    <a href="{{ $messages->nextPageUrl() }}" class="btn btn-gray-800">
+                    <a href="{{ $messages->nextPageUrl() }}" class="btn btn-gray-800 {{ $messages->onLastPage() ? 'disabled' : '' }}">
                         <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                   clip-rule="evenodd"/>
