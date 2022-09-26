74d9e7e98a4b14c4c354a30f167048bc436c0a00
RedouaneJnah98 <redouanejnah2000@gmail.com>
2022-09-26 12:42:27 +0100

    Middleware return type bug fixes.

--- app/Http/Middleware/EnsureAdminEmailIsVerified.php
+++ app/Http/Middleware/EnsureAdminEmailIsVerified.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -17,9 +18,9 @@ class EnsureAdminEmailIsVerified
      *
      * @param Request $request
      * @param Closure(Request): (Response|RedirectResponse) $next
-     * @return Response|RedirectResponse
+     * @return Response|RedirectResponse|JsonResponse
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         if (!$request->user() ||
             ($request->user() instanceof MustVerifyEmail &&
